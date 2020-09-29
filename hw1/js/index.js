80cf6abc45a0aa19c4f63f01152f780107e5dc9c
TemirlanDzodziev <uccm.timmy@gmail.com>
2020-09-29 17:27:10 +0300

    bug fixes

--- hw1/js/index.js
+++ hw1/js/index.js
@@ -5,15 +5,23 @@ const circle = document.querySelector('circle');
 
 const path = document.querySelector('path'); 
 const input = document.querySelector('input');
+const mood = document.getElementById('mood')
 
 let isTrue = false;
 
 const newValue = "M 150 200 Q 225 300 300 200";
 const oldValue = "M 150 200 Q 225 100 300 200";
+const newMood = "Happy"
+const oldMood = "Sad"
 
 input.addEventListener('click',()=> {
 
     isTrue = !isTrue
     console.log(isTrue);
     path.setAttribute("d",isTrue ? newValue : oldValue);
+    if (isTrue === true) {
+        mood.innerHTML = newMood
+    } else {
+        mood.innerHTML = oldMood
+    }
 })
